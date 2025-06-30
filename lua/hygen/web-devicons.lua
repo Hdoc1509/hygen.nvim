3369bc2a8356c8d335dd1ab7bae573e6c39d2968
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-30 18:11:18 -0500

    feat(devicons): add filetype icon

--- lua/hygen/web-devicons.lua
+++ lua/hygen/web-devicons.lua
@@ -3,14 +3,20 @@ local M = {}
 -- https://github.com/jondot/hygen/blob/master/hygen.io/src/css/custom.css#L9
 local main_color = "#f722b1"
 
+-- TODO: add get_icon() function
+-- similar to hygen-compatible() function from my nvim-config
+
 function M.setup()
-  require("nvim-web-devicons").set_icon({
+  local devicons = require("nvim-web-devicons")
+
+  devicons.set_icon({
     hygen = {
       icon = "", -- same as `ejs`
       color = main_color,
       name = "Hygen",
     },
   })
+  devicons.set_icon_by_filetype({ hygen_template = "hygen" })
 end
 
 return M
