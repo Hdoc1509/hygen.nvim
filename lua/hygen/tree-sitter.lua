ebe2dcf7f7cb28bd2a0c291be9d662d8f922a079
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-01 18:43:45 -0500

    feat(treesitter): add `inject-hygen-ejs!` directive
    remove per-parser injections

--- lua/hygen/tree-sitter.lua
+++ lua/hygen/tree-sitter.lua
@@ -1,14 +1,6 @@
 local ts_parsers = require("nvim-treesitter.parsers")
 local get_hygen_subext = require("hygen.utils").get_hygen_subext
 
----@class hygen.treesitter.injection.Config
----@field bash? boolean Enables `embedded_template` injection for `bash` parser
----@field markdown_inline? boolean Enables `embedded_template` injection for `markdown_inline` parser
----@field html? boolean Enables `embedded_template` injection for `html` parser
-
----@class hygen.treesitter.Config
----@field injection? hygen.treesitter.injection.Config Additional injections
-
 local M = {}
 
 -- see changed features in treesitter section
@@ -19,19 +11,7 @@ local ext_to_ft = {
   ts = "typescript",
 }
 
----@type hygen.treesitter.Config
-local default_config = {
-  injection = {
-    bash = true,
-    markdown_inline = true,
-    html = true,
-  },
-}
-
----@param config? hygen.treesitter.Config
-function M.setup(config)
-  config = vim.tbl_deep_extend("force", default_config, config or {})
-
+function M.setup()
   --- @class ParserConfig
   local parser_config = ts_parsers.get_parser_configs()
 
@@ -63,46 +43,16 @@ function M.setup(config)
     directive_options
   )
 
-  -- TODO: add `inject-hygen-ejs-to!` directive
-  -- remove `inject-hygen-[bash|markdown_inline|html]-ejs!` directives. these
-  -- should be manually added to custom queries of usuers
   -- NOTE: this will inject `embedded_template`, but its `injection` query uses
   -- `ruby` instead of `javascript`. will it require to use a custom directive
   -- to only inject `javascript` to `.ejs` and `.subext.hygen` files and `ruby`
   -- for the rest?
-
+  -- TODO: override `injections` query of `embedded_template` to apply
+  -- `javascript` or `ruby` parser depending on file extension
   vim.treesitter.query.add_directive(
-    "inject-hygen-bash-ejs!",
+    "inject-hygen-ejs!",
     function(_, _, bufnr, _, metadata)
-      if not config.injection.bash or get_hygen_subext(bufnr) == nil then
-        return
-      end
-
-      metadata["injection.language"] = "embedded_template"
-    end,
-    ---@diagnostic disable-next-line: param-type-mismatch
-    directive_options
-  )
-
-  vim.treesitter.query.add_directive(
-    "inject-hygen-markdown_inline-ejs!",
-    function(_, _, bufnr, _, metadata)
-      if
-        not config.injection.markdown_inline or get_hygen_subext(bufnr) == nil
-      then
-        return
-      end
-
-      metadata["injection.language"] = "embedded_template"
-    end,
-    ---@diagnostic disable-next-line: param-type-mismatch
-    directive_options
-  )
-
-  vim.treesitter.query.add_directive(
-    "inject-hygen-html-ejs!",
-    function(_, _, bufnr, _, metadata)
-      if not config.injection.html or get_hygen_subext(bufnr) == nil then
+      if get_hygen_subext(bufnr) == nil then
         return
       end
 
