8670d528ec0524658dbee44d81af034e653ab5be
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-11 10:21:09 -0500

    feat(scripts): use `trigger_release()` for initial release

--- scripts/release-manager.sh
+++ scripts/release-manager.sh
@@ -1,20 +1,20 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 changelog_file=$REPO_ROOT/CHANGELOG.md
 
+export REPO_ROOT
+
+source "$REPO_ROOT"/scripts/utils.sh
+
 if ! [[ -f $changelog_file ]]; then
   echo "[RELEASE]: Initial release!"
   echo -e '[RELEASE]: Breaking changes check will be skipped!\n'
 
-  pnpm changeset version
+  trigger_release
 
   sed -i "5 s/.*/### Initial release/" "$changelog_file"
 
   exit 0
 fi
-
-export REPO_ROOT
-
-source "$REPO_ROOT"/scripts/utils.sh
 
 previous_version=$(sed --quiet '3p' "$changelog_file" | awk '{ print $2 }')
 previous_major=$(cut --delimiter=. --fields=1 <<<"$previous_version")
