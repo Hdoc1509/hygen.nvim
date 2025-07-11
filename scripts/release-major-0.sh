8469e6d68a4eb2f3ebd5d822500dc603df995f84
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-11 17:33:36 -0500

    refactor(scripts): move util to get last tag out from `release_major_0()`

--- scripts/release-major-0.sh
+++ scripts/release-major-0.sh
@@ -1,13 +1,13 @@
 # inject:regex:
 unstable_message_regex='^\w+(\(.*\))?:!'
 
+_get_last_tag() { git describe --tags --abbrev=0; }
+
 release_major_0() {
   local previous_version=$1
 
-  get_last_tag() { git describe --tags --abbrev=0; }
-
   breaking_changes_count=$(
-    git log "$(get_last_tag)"..HEAD --oneline |
+    git log "$(_get_last_tag)"..HEAD --oneline |
       grep --count --extended-regexp "$unstable_message_regex"
   )
 
