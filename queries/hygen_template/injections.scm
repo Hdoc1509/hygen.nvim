8582a6c91152701870897dd743d6ca9ba871405c
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-13 12:53:32 -0500

    feat(queries): inject `regex` to `metadata.value` for some `metadata.key` nodes
    related to:
    https://github.com/Hdoc1509/tree-sitter-hygen-template/commit/0b8341b9a624a0fe55ecef2e77b33b78f830a416

--- queries/hygen_template/injections.scm
+++ queries/hygen_template/injections.scm
@@ -9,6 +9,13 @@
 ((code) @injection.content
   (#set! injection.language "javascript")
   (#set! injection.combined))
+(frontmatter
+  (metadata
+    ((key) @key
+      (#any-of? @key "after" "skip_if" "before"))
+    (value
+      ((string_value) @injection.content
+        (#set! injection.language "regex")))))
 
 ; dynamic injection
 ((content) @injection.content
