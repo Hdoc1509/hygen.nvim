cbf77dc2a28589b98b540a56c951ea5f02a20d07
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-13 12:47:10 -0500

    feat(queries): complete list of valid `metadata.key` nodes to be highlighted
    related to:
    https://github.com/Hdoc1509/tree-sitter-hygen-template/commit/fe12d5bafcd7abcf9c8d40bea6c37df9a8d549cd

--- queries/hygen_template/highlights.scm
+++ queries/hygen_template/highlights.scm
@@ -2,7 +2,8 @@
   (metadata
     ((key) @property
       (#any-of? @property
-        "to" "from" "force" "unless_exists" "inject" "after" "skip_if" "sh"))))
+        "to" "from" "force" "unless_exists" "inject" "after" "skip_if" "sh" "before" "prepend"
+        "append" "at_line"))))
 
 (frontmatter
   (metadata
