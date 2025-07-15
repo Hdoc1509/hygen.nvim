1d560d4f451a448a6871c75227d6db0aaf1e7bdc
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-15 16:59:30 -0500

    Revert "feat(devicons): retrieve icon data from buffer options and its first line"
    feature out of plugin scope
    
    This reverts commit af7cc91428b8e83b1674c0b07346179dfe151fe7.

--- lua/hygen/web-devicons.lua
+++ lua/hygen/web-devicons.lua
@@ -18,77 +18,27 @@ function M.setup()
   devicons.set_icon_by_filetype({ hygen_template = "hygen" })
 end
 
---Returns the `icon` from subextension, hygen `color` and `highlight` group.
---Fallbacks to `devicons` utilities if `filename` does not match naming
---convention
+--Returns the icon from subextension and hygen color
+--Fallbacks to `devicons.get_icon_color()` if `filename` does not match
+--naming convention
 ---@param filename string
----@param opts? { fullpath?: string, bufnr?: number}
----@return string icon, string color, string? hl
-function M.get_icon(filename, opts)
-  opts = opts or {}
-
+---@return string icon, string color
+function M.get_icon(filename)
   local subext = get_hygen_subext(filename)
   local ext = vim.fn.fnamemodify(filename, ":e")
 
-  -- TODO: improve this. reduce indentations
-
   if subext == nil and ext ~= "hygen" then
-    local initial_icon, initial_color = devicons.get_icon_color(filename, ext)
-
-    if initial_icon ~= nil and initial_color ~= nil then
-      return initial_icon, initial_color
-    end
-
-    if ext == "" then
-      local filetype = "unknown"
-
-      if type(opts.bufnr) == "number" then
-        filetype = vim.bo[opts.bufnr].filetype
-      elseif type(opts.fullpath) == "string" then
-        ---@type string[]
-        local first_line = vim.fn.readfile(opts.fullpath, "", 1)
-
-        if #first_line > 0 then
-          local shell_bin_hash = first_line[1]:match("^#!/bin/(%w+)")
-
-          if shell_bin_hash ~= nil then
-            filetype = shell_bin_hash
-          else
-            local shell_usr_bin_env_hash =
-              first_line[1]:match("^#!/usr/bin/env (%w+)")
-
-            if shell_usr_bin_env_hash ~= nil then
-              filetype = shell_usr_bin_env_hash
-            else
-              local ft_modeline = first_line[1]:match(".*vim:.*ft=(%w+)")
-
-              if ft_modeline ~= nil then
-                filetype = ft_modeline
-              end
-            end
-          end
-        end
-      end
-
-      local icon, color =
-        devicons.get_icon_color_by_filetype(filetype, { default = true })
-      local _, hl = devicons.get_icon_by_filetype(filetype, { default = true })
-
-      return icon, color, hl
-    end
-
-    return initial_icon, initial_color
+    return devicons.get_icon_color(filename, ext, { default = true })
   end
 
   local target_name = vim.fn.fnamemodify(filename, ":t:r")
-  local icon =
-    devicons.get_icon(target_name, vim.fn.fnamemodify(target_name, ":e"))
+  local icon = devicons.get_icon(
+    target_name,
+    vim.fn.fnamemodify(target_name, ":e"),
+    { default = true }
+  )
 
-  if icon == nil then
-    return devicons.get_icon_color(filename, ext)
-  else
-    return icon, main_color
-  end
+  return icon, main_color
 end
 
 return M
