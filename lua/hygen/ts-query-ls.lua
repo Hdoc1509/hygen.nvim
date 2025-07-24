be03ee4d19ef98cfd68c5ed7166491a21def7bd2
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-24 12:03:44 -0500

    feat(lsp): complete exported settings for `ts_query_ls`

--- lua/hygen/ts-query-ls.lua
+++ lua/hygen/ts-query-ls.lua
@@ -5,7 +5,27 @@ return {
         parameters = {
           { type = "capture", arity = "required" },
         },
-        description = 'Check if the given frontmatter has the "from" key',
+        description = 'Check if the given frontmatter has the `from` key',
+      },
+    },
+    valid_directives = {
+      ["inject-embedded_template"] = {
+        parameters = {
+          { type = "string", arity = "optional" },
+        },
+        description = "Directive to handle `ejs` and `erb` injections",
+      },
+      ["inject-hygen-ejs"] = {
+        parameters = {
+          { type = "string", arity = "optional" },
+        },
+        description = "Injects `ejs` in dynamic injections. Only have effect in `hygen` files.",
+      },
+      ["inject-hygen-tmpl"] = {
+        parameters = {
+          { type = "capture", arity = "required" },
+        },
+        description = "Directive to enable dynamic injections for `hygen` files.",
       },
     },
   },
