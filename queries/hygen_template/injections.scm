Hdoc1509/hygen.nvim

commit 02b860fc8cf76a02695de73c45e2313bd14b1fbc

refactor(queries): use `lua-match?` instead of custom predicate

queries/hygen_template/injections.scm
@@ -30,7 +30,7 @@
       (output_directive
         (code) @injection.content)
     ]
-    (#not-has-hygen-from-key? @frontmatter)
+    (#not-lua-match? @frontmatter "from:")
     (#set! injection.language "javascript")
     (#set! injection.combined)))
 
@@ -39,6 +39,6 @@
   (frontmatter)? @frontmatter
   (body
     ((content) @injection.content
-      (#not-has-hygen-from-key? @frontmatter)
+      (#not-lua-match? @frontmatter "from:")
       (#inject-hygen-tmpl! @frontmatter)
       (#set! injection.combined))))

lua/hygen/tree-sitter/init.lua
@@ -2,7 +2,6 @@ local M = {}
 
 function M.setup()
   require("hygen.tree-sitter.parser-info").setup()
-  require("hygen.tree-sitter.predicates").setup()
   require("hygen.tree-sitter.directives").setup()
 end
 

lua/hygen/tree-sitter/predicates.lua
@@ -1,34 +1,0 @@
----@type table<string, TSPredicate>
-local predicates = {
-  ["has-hygen-from-key?"] = function(match, _, bufnr, pred)
-    local filename = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
-    local ext = vim.fn.fnamemodify(filename, ":e")
-
-    if ext ~= "hygen" then
-      return
-    end
-
-    local frontmatter_capture_id = pred[2]
-    local frontmatter_node = match[frontmatter_capture_id]
-
-    if frontmatter_node == nil then
-      return false
-    end
-
-    local frontmatter_text =
-      vim.treesitter.get_node_text(frontmatter_node, bufnr)
-
-    return string.match(frontmatter_text, "\nfrom:") ~= nil
-  end,
-}
-
-local function setup()
-  local predicate_options = require("hygen.compat").predicate_options
-
-  for name, handler in pairs(predicates) do
-    ---@diagnostic disable-next-line: param-type-mismatch
-    vim.treesitter.query.add_predicate(name, handler, predicate_options)
-  end
-end
-
-return { setup = setup }

lua/hygen/ts-query-ls.lua
@@ -1,13 +1,5 @@
 return {
   settings = {
-    valid_predicates = {
-      ["has-hygen-from-key"] = {
-        parameters = {
-          { type = "capture", arity = "required" },
-        },
-        description = 'Check if the given frontmatter has the `from` key',
-      },
-    },
     valid_directives = {
       ["inject-embedded_template"] = {
         parameters = {

queries/hygen_template/highlights.scm
@@ -46,5 +46,5 @@
 
 (template
   (frontmatter) @frontmatter
-  ((body) @comment
-    (#has-hygen-from-key? @frontmatter)))
+  (#lua-match? @frontmatter "from:")
+  (body) @comment)

commit 6627f94c718736ad9c655a1c4dabd940cf06618e

fix(queries): injections without frontmatter
because `not-lua-match?` predicate is not triggered if optional node is not
present

bug introduced by:
https://github.com/Hdoc1509/hygen.nvim/commit/02b860fc8cf76a02695de73c45e2313bd14b1fbc

queries/hygen_template/injections.scm
@@ -22,7 +22,7 @@
         (#set! injection.language "javascript")))))
 
 (template
-  (frontmatter)? @frontmatter
+  (frontmatter) @frontmatter
   (body
     [
       (directive
@@ -34,11 +34,29 @@
     (#set! injection.language "javascript")
     (#set! injection.combined)))
 
-; dynamic injection
 (template
-  (frontmatter)? @frontmatter
+  .
+  (body
+    [
+      (directive
+        (code) @injection.content)
+      (output_directive
+        (code) @injection.content)
+    ]
+    (#set! injection.language "javascript")
+    (#set! injection.combined)))
+
+(template
+  (frontmatter) @frontmatter
   (body
     ((content) @injection.content
       (#not-lua-match? @frontmatter "from:")
-      (#inject-hygen-tmpl! @frontmatter)
+      (#inject-hygen-tmpl! "")
+      (#set! injection.combined))))
+
+(template
+  .
+  (body
+    ((content) @injection.content
+      (#inject-hygen-tmpl! "")
       (#set! injection.combined))))