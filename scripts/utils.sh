4be809796df52d7a12b87d1dd4ded9066a3da28c
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-12-07 13:11:08 -0500

    chore(scripts): remove unnecessary reminder message
    plugin's package is not present in pnpm-lock.yaml

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -40,8 +40,6 @@ reminder_message() {
   local new_version
   new_version=$(sed --quiet '3p' "$CHANGELOG_FILE" | awk '{ print $2 }')
 
-  echo && info_log "If all changes are correct, update lock file by running:"
-  command_snippet "pnpm" "install"
   echo && warn_log "Don't forget to commit the changes:"
   command_snippet "git" "commit -m 'chore: release v$new_version'"
   echo && warn_log "Don't forget to generate git tags:"
